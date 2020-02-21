e4d309e9b692e47885866b32e170f73b9a3cc0a2
pulibhavana <bhavana.puli@thoughtworks.com>
2020-02-21 16:26:44 +0530

    Add new function getIndex

--- IndexOfNumber.js
+++ IndexOfNumber.js
@@ -1,11 +1,17 @@
 function main()
 {
     numbersList = [10,20,30,10,10,20,40,40,-10]
-    result = numbersList.findIndex(number => number == 40)
+    result = getIndex()
     if(result != -1)
         console.log("The index of number is " +result)
     else
         console.log("The number doesnt exist")
+}
+
+function getIndex()
+{
+    result = numbersList.findIndex(number => number == 40)
+    return result
 }
 
 main()
