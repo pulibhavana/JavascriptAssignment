ee3af96ad5c5a30da239729507eaca4582a62b40
pulibhavana <bhavana.puli@thoughtworks.com>
2020-02-21 16:29:13 +0530

    Add new function checkIfEven

--- CountEvenNumbers.js
+++ CountEvenNumbers.js
@@ -9,7 +9,7 @@ function count(numberslist)
     return count
 }
 
-function iseven(num)
+function isEven(num)
 {
     return (num%2 == 0);
 }
@@ -18,7 +18,12 @@ function main() {
     var numbers = [10, 35, 54, 27, 1, 8];
     console.log("The given list is " + numbers)
     console.log("The count of even numbers in the list is")
-    result = numbers.filter(number => number%2 == 0)
+    result = numbers.filter(checkIfIsEven)
     console.log(result.length)
+}
+
+function checkIfIsEven(element)
+{
+    return element % 2 == 0
 }
 main()
